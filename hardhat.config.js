acc9df6e2a03dd059daf84f8a1f67a23eb6bfe9d
VadneyK <57117207+VadneyK@users.noreply.github.com>
2021-06-01 21:06:55 -0700

    Add files via upload

---
+++ hardhat.config.js
@@ -0,0 +1,20 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+require("@nomiclabs/hardhat-ethers");
+
+module.exports = {
+  solidity: "0.8.1",
+
+  networks: {
+    //Dusty
+    dusty: {
+      url: "https://rpc.dusty.plasmnet.io:8545",
+      chainid: 80,
+      accounts: [
+        "0x60ed0dd24087f00faea4e2b556c74ebfa2f0e705f8169733b01530ce4c619883",
+      ],
+    },
+  },
+};
