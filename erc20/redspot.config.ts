4581415e538b3913c95878413d3cb183d362fad4
Kentaro Vadney <kentarovadney@Kentaros-MacBook-Air.local>
2021-06-04 09:04:28 -0700

    tried suggestion from redspot

--- erc20/redspot.config.ts
+++ erc20/redspot.config.ts
@@ -1,32 +1,41 @@
-import { RedspotUserConfig } from 'redspot/types';
-import '@redspot/patract';
-import '@redspot/chai';
-import '@redspot/gas-reporter';
+import { RedspotUserConfig } from "redspot/types";
+import "@redspot/patract";
+import "@redspot/chai";
+import "@redspot/gas-reporter";
+import types from "@plasm/types";
 
 export default {
-  defaultNetwork: 'development',
+  defaultNetwork: "development",
   contract: {
     ink: {
-      toolchain: 'nightly',
-      sources: ['contracts/**/*']
-    }
+      toolchain: "nightly",
+      sources: ["contracts/**/*"],
+    },
   },
   networks: {
+    plasm: {
+      endpoint: "ws://127.0.0.1:9944",
+      gasLimit: "400000000000",
+      accounts: ["//Alice"],
+      types: {
+        ...types.dustyDefinitions,
+      },
+    },
     development: {
-      endpoint: 'ws://127.0.0.1:9944',
+      endpoint: "ws://127.0.0.1:9944",
       types: {},
-      gasLimit: '400000000000',
+      gasLimit: "400000000000",
       explorerUrl:
-        'https://polkadot.js.org/apps/#/explorer/query/?rpc=ws://127.0.0.1:9944/'
+        "https://polkadot.js.org/apps/#/explorer/query/?rpc=ws://127.0.0.1:9944/",
     },
     substrate: {
-      endpoint: 'ws://127.0.0.1:9944',
-      gasLimit: '400000000000',
-      accounts: ['//Alice'],
-      types: {}
-    }
+      endpoint: "ws://127.0.0.1:9944",
+      gasLimit: "400000000000",
+      accounts: ["//Alice"],
+      types: {},
+    },
   },
   mocha: {
-    timeout: 60000
-  }
+    timeout: 60000,
+  },
 } as RedspotUserConfig;
--- erc20/scripts/deploy.ts
+++ erc20/scripts/deploy.ts
@@ -1,29 +1,48 @@
-import { patract, network } from 'redspot';
+import { patract, network } from "redspot";
 
 const { getContractFactory } = patract;
 const { createSigner, keyring, api } = network;
+const { extendEnvironment } = require("redspot/config");
+import { WsProvider } from "@polkadot/api";
+import type { RegistryTypes } from "@polkadot/types/types";
+import typeDefs from "@plasm/types";
+import { ApiPromise } from "redspot/types";
+
+const { ApiPromise } = require("@polkadot/api");
+
+extendEnvironment((rse) => {
+  const provider = new WsProvider("ws://127.0.0.1:9944");
+  const types = typeDefs.dustyDefinitions;
+  const api = new ApiPromise({
+    provider,
+    types: {
+      ...(types as RegistryTypes),
+    },
+  });
+  rse.api = api;
+});
 
 const uri =
-  'bottom drive obey lake curtain smoke basket hold race lonely fit walk//Alice';
+  "bottom drive obey lake curtain smoke basket hold race lonely fit walk//Alice";
 
 async function run() {
   await api.isReady;
 
   const signer = createSigner(keyring.createFromUri(uri));
-  const contractFactory = await getContractFactory('erc20', signer);
+  const contractFactory = await getContractFactory("erc20", signer);
 
   const balance = await api.query.system.account(signer.address);
 
-  console.log('Balance: ', balance.toHuman());
+  console.log("Balance: ", balance.toHuman());
 
-  const contract = await contractFactory.deployed('new', '1000000', {
-    gasLimit: '200000000000',
-    value: '100000000000'
+  const contract = await contractFactory.deployed("new", "1000000", {
+    gasLimit: "200000000000",
+    value: "100000000000",
   });
 
-  console.log('');
+  console.log("");
   console.log(
-    'Deploy successfully. The contract address: ',
+    "Deploy successfully. The contract address: ",
     contract.address.toString()
   );
 
