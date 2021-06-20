af027d089805643682edbf7def6ff0d2f858c372
VadneyK <57117207+VadneyK@users.noreply.github.com>
2021-06-20 16:49:24 -0700

    default alice and local nodes (#2)
    Co-authored-by: Kentaro Vadney <kentarovadney@Kentaros-MacBook-Air.local>

--- erc20/scripts/deploy.ts
+++ erc20/scripts/deploy.ts
@@ -2,18 +2,16 @@ import { patract, network } from "redspot";
 require("dotenv").config();
 
 const { getContractFactory } = patract;
-const { createSigner, keyring, api } = network;
+const { api } = network;
 
-const uri: string = process.env.MNEMONIC!;
+const signer = "5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY"; // Alice Address
 
 async function run() {
   await api.isReady;
 
-  const signer = createSigner(keyring.createFromUri(uri));
   const contractFactory = await getContractFactory("erc20", signer);
 
-  const balance = await api.query.system.account(signer.address);
-
+  const balance = await api.query.system.account(signer);
   console.log("Balance: ", balance.toHuman());
 
   const contract = await contractFactory.deployed("new", "1000000", {
